98af70621016e4dcd8b4c8feae64662e43e8410b
u.dukuev <u.dukuev@ase-ec.ru>
2024-09-25 11:07:15 +0300

    remove_duplicate_2

--- server/src/ru_server/ChatServer.java
+++ server/src/ru_server/ChatServer.java
@@ -1,7 +1,29 @@
 package ru_server;
 
-public class ChatServer {
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class ChatServer implements TCPConnectionListener {
+
     public static void main(String[] args) {
+        new ChatServer();
+    }
+
+    private final ArrayList<TCPConnection> connections = new ArrayList<>();
+
+    private ChatServer() {
+        System.out.println("Server running...");
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            while (true) {
+                try {
+                    new TCPConnection(this, serverSocket.accept());
+                } catch (IOException e) {
+                    System.err.println("TCPConnection exception: " + e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
