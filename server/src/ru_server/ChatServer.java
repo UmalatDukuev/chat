222a285717b06841fe93d9417b9f3a2a1296984e
u.dukuev <u.dukuev@ase-ec.ru>
2024-09-28 16:24:37 +0300

    1

--- server/src/ru_server/ChatServer.java
+++ server/src/ru_server/ChatServer.java
@@ -7,13 +7,15 @@ import java.util.ArrayList;
 import ru_network.TCPConnection;
 import ru_network.TCPConnectionListener;
 
-public class ChatServer implements TCPConnectionListener{
+public class ChatServer implements TCPConnectionListener {
 
     public static void main(String[] args) {
         new ChatServer();
     }
 
+
     private final ArrayList<TCPConnection> connections = new ArrayList<>();
+
     private ChatServer() {
         System.out.println("Server running...");
         try (ServerSocket serverSocket = new ServerSocket(8189)) {
@@ -32,22 +34,30 @@ public class ChatServer implements TCPConnectionListener{
 
     @Override
     public void onConnectionReady(TCPConnection tcpConnection) {
-
+        connections.add(tcpConnection);
+        sendToAllConnections("Client connected: " + tcpConnection);
     }
 
     @Override
     public void onReceiveString(TCPConnection tcpConnection, String value) {
-
+        sendToAllConnections(value);
     }
 
     @Override
     public void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+        sendToAllConnections("Client disconnected: " + tcpConnection);
 
     }
 
     @Override
     public void onException(TCPConnection tcpConnection, Exception e) {
-
+        System.out.println("TCPConnection exception: " + e);
     }
 
+    private void sendToAllConnections(String value) {
+    for (int i = 0; i < connections.size(); i++){
+        connections.get(i).sendString(value);
+        }
+    }
 }
